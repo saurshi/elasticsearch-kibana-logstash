853aaceea7c33bdff7393bc649beae316a984d4c
Sergey  Shitenkov <s@shitenkov.ru>
2021-02-11 01:12:07 +0300

    fix.chmod+

--- chmod.sh
+++ chmod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dir=$PWD
 
+#chown -R  "$dir";
 chown -R  "$dir";
 find "$dir" -type d -exec chmod 755 '{}' \;
-find "$dir" -type f -exec chmod 644 '{}' \;
+find "$dir" -type f -exec chmod 755 '{}' \;
