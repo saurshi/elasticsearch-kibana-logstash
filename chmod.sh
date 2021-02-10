f949271a115e162389fbc3e04ee6f44a6eed9f5d
Sergey  Shitenkov <s@shitenkov.ru>
2021-02-11 01:16:05 +0300

    chmod-norm

--- chmod.sh
+++ chmod.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-dir=$PWD
-
-#chown -R  "$dir";
-chown -R  "$dir";
-find "$dir" -type d -exec chmod 755 '{}' \;
-find "$dir" -type f -exec chmod 755 '{}' \;
+find  -type d -exec chmod 0755 '{}' \;
+find  -type f -exec chmod 0644 '{}' \;
